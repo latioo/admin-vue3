79fa327ca1944aaf6aaef27fef80fa6822eb7169
lovelyorg <lovelyorg@126.com>
2022-05-17 15:33:22 +0800

    固定左侧菜单

--- src/layouts/AsideBar.vue
+++ src/layouts/AsideBar.vue
@@ -1,56 +1,67 @@
 <template>
-  <el-aside class="aside">
-    <span class="logo">logo</span>
-    <div class="menu">
-      <el-menu default-active="/" class="el-menu-vertical" :collapse="isExpand" :router="true">
-        <el-menu-item index="/">
+  <div>
+    <div class="fixed-stuff" :style="{ width: `${isExpand ? '208' : '70'}px`, overflow: 'hidden' }"></div>
+    <div class="side-fixed">
+      <el-aside class="el-aside">
+        <span class="logo">logo</span>
+        <div class="menu">
+          <el-menu default-active="/" class="el-menu-vertical" :collapse="!isExpand" :router="true">
+            <el-menu-item index="/">
+              <el-icon>
+                <HomeFilled />
+              </el-icon>
+              <template #title>
+                Home
+              </template>
+            </el-menu-item>
+            <el-sub-menu index="2">
+              <template #title>
+                <el-icon>
+                  <location />
+                </el-icon>
+                <span>Navigator</span>
+              </template>
+              <el-menu-item index="/page1">
+                Page1
+              </el-menu-item>
+              <el-menu-item index="/page2">
+                Page2
+              </el-menu-item>
+            </el-sub-menu>
+            <el-menu-item v-for="item in toMuchMenusFortest" :index="item" :key="item">
+              {{ "test" + item }}
+            </el-menu-item>
+          </el-menu>
+        </div>
+        <div class="expand" @click="setExpand">
           <el-icon>
-            <HomeFilled />
+            <Expand v-if="!isExpand" />
+            <Fold v-if="isExpand" />
           </el-icon>
-          <template #title>
-            Home
-          </template>
-        </el-menu-item>
-        <el-sub-menu index="2">
-          <template #title>
-            <el-icon>
-              <location />
-            </el-icon>
-            <span>Navigator</span>
-          </template>
-          <el-menu-item index="/page1">
-            Page1
-          </el-menu-item>
-          <el-menu-item index="/page2">
-            Page2
-          </el-menu-item>
-        </el-sub-menu>
-        <el-menu-item v-for="item in toMuchMenusFortest" :index="item" :key="item">
-          {{ "test" + item }}
-        </el-menu-item>
-      </el-menu>
+        </div>
+      </el-aside>
     </div>
-    <div class="expand" @click="setExpand">
-      <el-icon>
-        <Expand v-if="isExpand" />
-        <Fold v-if="!isExpand" />
-      </el-icon>
-    </div>
-  </el-aside>
+  </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { Menu as IconMenu, Location, HomeFilled, Expand, Fold } from '@element-plus/icons-vue'
 
-const isExpand = ref(false)
+const isExpand = ref(true)
 const setExpand = () => { isExpand.value = !isExpand.value }
 
 const toMuchMenusFortest = Array(11).fill(null).map((_, i) => i + '')
 </script>
 
 <style lang="scss" scoped>
-.aside {
+.side-fixed {
+  position: fixed;
+  background-color: #fff; // 和 z-index 配合, 收起菜单动画时遮住右侧内容
+  z-index: 10;
+}
+
+.el-aside {
   display: flex;
   flex-direction: column;
   height: 100vh;
@@ -73,18 +84,6 @@ const toMuchMenusFortest = Array(11).fill(null).map((_, i) => i + '')
     border-radius: 3px;
     -webkit-box-shadow: inset 0 0 5px rgb(0 21 41 / 5%);
   }
-
-  // ::-webkit-scrollbar-thumb {
-  //   background: hsla(0, 0%, 100%, .2);
-  //   border-radius: 3px;
-  //   -webkit-box-shadow: inset 0 0 5px hsl(0deg 0% 100% / 5%);
-  // }
-
-  // ::-webkit-scrollbar-track {
-  //   background: hsla(0, 0%, 100%, .15);
-  //   border-radius: 3px;
-  //   -webkit-box-shadow: inset 0 0 5px rgb(37 37 37 / 5%);
-  // }
 
   .logo {
     padding: 16px;
--- src/views/Page1.vue
+++ src/views/Page1.vue
@@ -1,3 +1,22 @@
 <template>
   <h1>Page1</h1>
+  <h1>Page1</h1>
+  <h1>Page1</h1>
+  <h1>Page1</h1>
+  <h1>Page1</h1>
+  <h1>Page1</h1>
+  <h1>Page1</h1>
+  <h1>Page1</h1>
+  <h1>Page1</h1>
+  <h1>Page1</h1>
+  <h1>Page1</h1>
+  <h1>Page1</h1>
+  <h1>Page1</h1>
+  <h1>Page1</h1>
+  <h1>Page1</h1>
+  <h1>Page1</h1>
+  <h1>Page1</h1>
+  <h1>Page1</h1>
+  <h1>Page1</h1>
+  <h1>Page1</h1>
 </template>
